456b37e00bfaa3e702d535e0c8811f0da49e3169
k.kapustin <k.v.kapustin@gmail.com>
2024-03-03 16:43:49 +0300

    Добавили сетап и README.md

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vink_data_transform',
+    version='0.1.0',
+    author='Капустин Кирилл',
+    author_email='k.kapustin@vink.ru',
+    description='Набор инструментов для трансформации данных, включая работу с базами данных, файлами и XML.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/kapustinkirill/vink_data_transform',
+    packages=find_packages(),
+    install_requires=[
+        # Зависимости проекта, например:
+        'psycopg2-binary',
+        'lxml',
+        # Другие зависимости
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
