1e61a3404a9a020ccbdba56c0ce1373180cefe0b
k.kapustin <k.v.kapustin@gmail.com>
2024-03-03 11:03:31 +0300

    xml  обработчик

--- trasformation/db_connectors.py
+++ trasformation/db_connectors.py
@@ -1,9 +1,6 @@
-import json
 from contextlib import contextmanager
 import psycopg2
 from psycopg2.extras import execute_batch
-
-from config import db_details
 
 
 class DatabaseManager:
@@ -28,67 +25,6 @@ class DatabaseManager:
         finally:
             cur.close()
             conn.close()
-
-    # def insert_entities_in_batches(cur, table_name, entities_data, schema, conflict_target):
-    #     # Строим список столбцов и placeholders из схемы
-    #     columns = [col for col, dtype in schema]
-    #     placeholders = ", ".join(["%s" for _ in schema])
-    #     columns_list = ", ".join(columns)
-    #
-    #     # Определяем столбцы для обновления, исключая conflict_target
-    #     update_columns = [col for col in columns if col != conflict_target]
-    #
-    #     # Формируем строку SET для обновления
-    #     update_set = ", ".join([f"{col} = EXCLUDED.{col}" for col in update_columns])
-    #
-    #     # Формируем действие при конфликте
-    #     conflict_action = f"ON CONFLICT ({conflict_target}) DO UPDATE SET {update_set}" if conflict_target else "ON CONFLICT DO NOTHING"
-    #
-    #     # Формируем и выполняем запрос
-    #     insert_query = f"""
-    #     INSERT INTO {table_name} ({columns_list})
-    #     VALUES ({placeholders})
-    #     {conflict_action};
-    #     """
-    #
-    #     chunk_size = 1000
-    #     for i in range(0, len(entities_data), chunk_size):
-    #         chunk = entities_data[i:i + chunk_size]
-    #         #         print(f"Insert query: {insert_query}")  # Debugging
-    #         #         print(f"Sample chunk: {chunk}")  # Debugging, print first element of the chunk
-    #         try:
-    #             execute_batch(cur, insert_query, chunk)
-    #
-    #         except Exception as e:
-    #             print(f"Ошибка при вставке данных в {table_name}: {e}")
-    #             print(f"Данные, вызвавшие ошибку: {chunk[0]}")
-    #             print(f"Запрос, вызвавший ошибку: {insert_query}")
-    #             break  # Прерываем выполнение, чтобы избежать дальнейших ошибок
-
-    # def insert_entities_in_batches(self, cur, table_name, entities_data, config_key):
-    #     config = self.config
-    #     fields_config = config['fields']
-    #     computed_fields_config = config.get('computed_fields', [])
-    #
-    #     columns = [field['dest'] for field in fields_config]
-    #     placeholders = ", ".join(["%s" for _ in fields_config + computed_fields_config])
-    #     columns_list = ", ".join(columns + [cf['dest'] for cf in computed_fields_config])
-    #
-    #     insert_query = f"INSERT INTO {table_name} ({columns_list}) VALUES ({placeholders})"
-    #
-    #     chunk_size = 1000
-    #     for i in range(0, len(entities_data), chunk_size):
-    #         chunk = entities_data[i:i + chunk_size]
-    #         values = []
-    #         for item in chunk:
-    #             row = [item[field['source']] for field in fields_config]
-    #             # Добавление вычисляемых полей
-    #             for comp_field in computed_fields_config:
-    #                 compute_func = getattr(self, comp_field['compute'])
-    #                 row.append(compute_func())
-    #             values.append(tuple(row))
-    #
-    #         execute_batch(cur, insert_query, values)
 
     def insert_entities_in_batches(self, cur, entities_data ):
         config = self.config
@@ -121,16 +57,3 @@ class DatabaseManager:
             self.insert_entities_in_batches(cur, entites_data)
 
 
-if __name__ == '__main__':
-    with open('../config.json', 'r', encoding='utf-8') as config_file:
-        config = json.load(config_file)
-
-
-    entites_data = [
-    {'ДатаПродажи': '12.01.2024 12:16:25', 'Номенклатура': 'Холст Indorian натуральный выбеленный, 65% хлопок, 350 г, 1,07*30 м, матовый', 'НоменклатураКод': 'о8750', 'НоменклатураНаименование': 'Холст Indorian натуральный выбеленный, 65% хлопок, 350 г, 1,07*30 м, матовый', 'НоменклатураГУИД': 'f4efd4a5-850f-11ed-af54-000c29083909', 'Количество': '1', 'СуммаПродажи': '124,47', 'ЗаказПокупателя': 'Заказ покупателя Г-Л01709 от 12.01.2024 12:11:09', 'ЗаказПокупателяНомер': 'Г-Л01709', 'ЗаказПокупателяОрганизация': 'Винк ООО', 'ОрганизацияГУИД': '196e0c68-c4d2-11e1-8896-005056c00008', 'ЗаказГУИД': '34142cb0-b119-11ee-bfe4-000c294ae96a', 'ЗаказПокупателяКонтрагент': 'ТОЧКА ОТРЫВА ООО (Спб)', 'КонтрагентГУИД': 'c2cb1712-4047-11e7-bffb-000c293bb9ea', 'ЗаказПокупателяКонтрагентПартне': None, 'ПартнерГУИД': 'a5fc7669-c661-11e5-a1ba-000c293bb9ea', 'ЗаказПокупателяСклад': 'Петербург', 'ЗаказПокупателяСкладРезки': 'Нет', 'ЗаказПокупателяПодразделение': 'ОП Шушары', 'ЗаказПокупателяСуммаДокумента': '26\xa0551,08', 'ЗаказПокупателяВалютаДокумента': 'RUB', 'ЗаказПокупателяВидОплаты': '1', 'ЗаказПокупателяСпособОплаты': 'По предварительной дог-сти (ИНАЧЕ)', 'ЗаказПокупателяПоИнтернетЗаявке': 'Нет', 'ЗаказПокупателяКонтрагентОсновн': None, 'ЗаказПокупателяДоставка': 'Да', 'ЗаказПокупателяРегионДоставки': 'Санкт-Петербург г', 'ЗаказПокупателяАдресДоставки': '198095, Санкт-Петербург г, Ивана Черных ул, дом № 29', 'ГородДоставки': '', 'РайонДоставки': '', 'ПоселокДоставки': '', 'УлицаДоставки': 'Ивана Черных ул', 'ЗаказПокупателяГрузополучатель': '', 'ЗаказПокупателяИсполнитель': 'Габо Валерия Олеговна', 'Возврат': False, 'hash': '6344a2eeaa91f5906c14883c022f400ff396c26469f4932fa0a942a0abf71ef1'},
-    {'ДатаПродажи': '12.01.2024 12:16:25', 'Номенклатура': 'Холст Indorian натуральный выбеленный, 65% хлопок, 350 г, 1,27*30 м, матовый', 'НоменклатураКод': 'о6176', 'НоменклатураНаименование': 'Холст Indorian натуральный выбеленный, 65% хлопок, 350 г, 1,27*30 м, матовый', 'НоменклатураГУИД': 'd1fb57e2-4c51-11ec-b179-000c29083909', 'Количество': '1', 'СуммаПродажи': '147,74', 'ЗаказПокупателя': 'Заказ покупателя Г-Л01709 от 12.01.2024 12:11:09', 'ЗаказПокупателяНомер': 'Г-Л01709', 'ЗаказПокупателяОрганизация': 'Винк ООО', 'ОрганизацияГУИД': '196e0c68-c4d2-11e1-8896-005056c00008', 'ЗаказГУИД': '34142cb0-b119-11ee-bfe4-000c294ae96a', 'ЗаказПокупателяКонтрагент': 'ТОЧКА ОТРЫВА ООО (Спб)', 'КонтрагентГУИД': 'c2cb1712-4047-11e7-bffb-000c293bb9ea', 'ЗаказПокупателяКонтрагентПартне': None, 'ПартнерГУИД': 'a5fc7669-c661-11e5-a1ba-000c293bb9ea', 'ЗаказПокупателяСклад': 'Петербург', 'ЗаказПокупателяСкладРезки': 'Нет', 'ЗаказПокупателяПодразделение': 'ОП Шушары', 'ЗаказПокупателяСуммаДокумента': '26\xa0551,08', 'ЗаказПокупателяВалютаДокумента': 'RUB', 'ЗаказПокупателяВидОплаты': '1', 'ЗаказПокупателяСпособОплаты': 'По предварительной дог-сти (ИНАЧЕ)', 'ЗаказПокупателяПоИнтернетЗаявке': 'Нет', 'ЗаказПокупателяКонтрагентОсновн': None, 'ЗаказПокупателяДоставка': 'Да', 'ЗаказПокупателяРегионДоставки': 'Санкт-Петербург г', 'ЗаказПокупателяАдресДоставки': '198095, Санкт-Петербург г, Ивана Черных ул, дом № 29', 'ГородДоставки': '', 'РайонДоставки': '', 'ПоселокДоставки': '', 'УлицаДоставки': 'Ивана Черных ул', 'ЗаказПокупателяГрузополучатель': '', 'ЗаказПокупателяИсполнитель': 'Габо Валерия Олеговна', 'Возврат': False, 'hash': 'dc7d183b23e261858fdb2345053e6e46a7a896c9b2840df50cc7430910617a5e'}
-
-    ]
-
-    db_connector = DatabaseManager(config["SalesProcessing"], **db_details)
-    db_connector.insert_data(entites_data)
---
+++ trasformation/xml_reader.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+from io import BytesIO
+from lxml import etree
+import io
+import xml.etree.ElementTree as ET
+import json
+
+def clean_and_parse_xml(file_buffer):
+    file_buffer.seek(0)
+    content = file_buffer.read()
+    content = content.lstrip(b'\xef\xbb\xbf').lstrip(b'\t\r\n')
+    file_like_object = io.BytesIO(content)
+    tree = etree.parse(file_like_object)
+    return tree
+
+class XMLParser:
+    def __init__(self, xml_path: str):
+        self.xml_path = xml_path
+
+    def clean_content(self, content: bytes) -> bytes:
+        return content.lstrip(b'\xef\xbb\xbf').lstrip(b'\t\r\n')
+
+    def parse(self) -> str:
+        with open(self.xml_path, 'rb') as file:
+            raw_content = file.read()
+            cleaned_content = self.clean_content(raw_content)
+            file_like_object = BytesIO(cleaned_content)
+            tree = ET.parse(file_like_object)
+            root = tree.getroot()
+
+
+        # Рекурсивная функция для преобразования XML в словарь
+        def element_to_dict(element):
+            # Создаем словарь атрибутов элемента
+            dict_attrs = element.attrib
+            if dict_attrs:
+                dict_data = {element.tag: {"attributes": dict_attrs}}
+            else:
+                dict_data = {element.tag: {}}
+
+            children = list(element)
+            if children:
+                dd = defaultdict(list)
+                for dc in map(element_to_dict, children):
+                    for k, v in dc.items():
+                        dd[k].append(v)
+                dict_data[element.tag].update({k: v[0] if len(v) == 1 else v for k, v in dd.items()})
+            elif not element.text or not element.text.strip():
+                # Если у элемента нет дочерних элементов и текста, то возвращаем только атрибуты
+                return dict_data
+            else:
+                # Если у элемента нет дочерних элементов, но есть текст, добавляем текст в словарь
+                text = element.text.strip()
+                dict_data[element.tag] = text if not dict_attrs else {"attributes": dict_attrs, "text": text}
+
+            return dict_data
+
+        # Преобразование корневого элемента XML в словарь
+        result_dict = element_to_dict(root)
+
+        # Преобразование словаря в JSON
+        json_data = json.dumps(result_dict, indent=4, ensure_ascii=False)
+        return json_data
+
